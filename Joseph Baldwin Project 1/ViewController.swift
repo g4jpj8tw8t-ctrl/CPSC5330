4847e7652d5c8570ef464894ccb0a1ecb841d853
Joseph Baldwin <jgb0041@auburn.edu>
2026-05-19 14:42:32 -0400

    Initial Commit

---
+++ Joseph Baldwin Project 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Joseph Baldwin Project 1
+//
+//  Created by J B on 5/19/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
